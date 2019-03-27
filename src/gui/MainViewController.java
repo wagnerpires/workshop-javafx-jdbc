7bb10c8d8c9a29eb6e707ad96ce6932fbd32ecd6
Wagner Pires <wagnerpires@hotmail.com>
2019-03-27 14:06:10 -0300

    Initializing action as parameter

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -3,6 +3,7 @@ package gui;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.function.Consumer;
 
 import application.Main;
 import gui.util.Alerts;
@@ -35,12 +36,15 @@ public class MainViewController implements Initializable {
 	
 	@FXML
 	private void onMenuItemDepartamentosAction() {
-		loadView2("/gui/DepartmentList.fxml");
+		loadView("/gui/DepartmentList.fxml", (DepartmentListController controller) -> {
+			controller.setDepartmentService(new DepartmentService());
+			controller.updateTableView();			
+		});
 	}
 
 	@FXML
 	private void onMenuItemSobreAction() {
-		loadView("/gui/About.fxml");
+		loadView("/gui/About.fxml", x -> {});
 	}
 	
 	
@@ -49,25 +53,7 @@ public class MainViewController implements Initializable {
 		
 	}
 	
-	private void loadView(String absoluteName) {
-		try {
-			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
-			VBox newVBox = loader.load();
-			
-			Scene mainScene = Main.getMainScene();
-			VBox mainVBox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();
-			
-			Node mainMenu = mainVBox.getChildren().get(0);
-			mainVBox.getChildren().clear();
-			mainVBox.getChildren().add(mainMenu);
-			mainVBox.getChildren().addAll(newVBox.getChildren());
-		}
-		catch (IOException e) {
-			Alerts.showAlert("Erro no carregamento", "Erro carregando a página", e.getMessage(), AlertType.ERROR);
-		}
-	}
-	
-	private void loadView2(String absoluteName) {
+	private synchronized <T> void loadView(String absoluteName, Consumer<T> initializingAction) {
 		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
 			VBox newVBox = loader.load();
@@ -80,12 +66,12 @@ public class MainViewController implements Initializable {
 			mainVBox.getChildren().add(mainMenu);
 			mainVBox.getChildren().addAll(newVBox.getChildren());
 			
-			DepartmentListController controller = loader.getController();
-			controller.setDepartmentService(new DepartmentService());
-			controller.updateTableView();
+			T controller = loader.getController();
+			initializingAction.accept(controller);
+			
 		}
 		catch (IOException e) {
-			Alerts.showAlert("Erro no carregamento", "Erro carregando a página", e.getMessage(), AlertType.ERROR);
+			Alerts.showAlert("Erro", "Erro carregando a página", e.getMessage(), AlertType.ERROR);
 		}
 	}
 }
--- src/model/services/DepartmentService.java
+++ src/model/services/DepartmentService.java
@@ -9,9 +9,13 @@ public class DepartmentService {
 	
 	public List<Department> findAll() {
 		List<Department> list = new ArrayList<>();
-		list.add(new Department(1, "Books"));
-		list.add(new Department(2, "Computers"));
-		list.add(new Department(3, "Eletronics"));
+		list.add(new Department(1, "Coordenação do Centro Integrado de Gestão Estratégica - CIGE"));
+		list.add(new Department(2, "Coordenação de Aviação Operacional - CAOP"));
+		list.add(new Department(3, "Serviço de Proteção ao Depoente Especial - SPDE"));
+		list.add(new Department(3, "Divisão de Segurança de Dignitário e Proteção ao Depoente Especial - DSDE"));
+		list.add(new Department(3, "Coordenação de Segurança em Grandes Eventos - CGE"));
+		list.add(new Department(3, "Serviço de Apoio Administrativo - SAD"));
+		
 		return list;
 	}
 }
